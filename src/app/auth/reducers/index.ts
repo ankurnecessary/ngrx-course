75dc613d4ce8f97845a17eeca7ce399cea93b9b2
Ankur Gupta <ankurnecessary@gmail.com>
2023-07-14 20:10:00 +0930

    Lecture 15: Added code till lecture 15. Here feature selectors are covered

---
+++ src/app/auth/reducers/index.ts
@@ -0,0 +1,28 @@
+import {
+  createReducer,
+  on
+} from '@ngrx/store';
+import { User } from '../model/user.model';
+import { authActions } from '../action-types';
+
+export interface AuthState {
+  user: User
+}
+
+export const initialAuthState: AuthState = {
+  user: undefined
+};
+
+export const authReducer = createReducer(
+
+  initialAuthState,
+
+  on(authActions.login, (state, action) => {
+    return {
+      user: action.user
+    }
+  })
+
+)
+
+
