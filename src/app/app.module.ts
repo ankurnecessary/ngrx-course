3d101ce046b3109d7a23afa20b2526f28c2528f6
Ankur Gupta <ankurnecessary@gmail.com>
2023-07-21 18:25:15 +0930

    Lecture 23: NgRx runtime checks - How do they work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -55,7 +55,15 @@ const routes: Routes = [
     MatListModule,
     MatToolbarModule,
     AuthModule.forRoot(),
-    StoreModule.forRoot(reducers, { metaReducers }),
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictActionImmutability: true,
+        strictActionSerializability: true,
+        strictStateImmutability: true,
+        strictStateSerializability: true
+      }
+    }),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
     EffectsModule.forRoot([]),
     StoreRouterConnectingModule.forRoot({
